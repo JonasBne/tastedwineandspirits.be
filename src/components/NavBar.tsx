9b5051c59b5c144122f860beed48c6b5039d3440
Jonas Boone <boone.jonas@outlook.be>
2023-07-01 18:28:08 +0200

    feat: add tasting cards

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -33,7 +33,7 @@ const NavBar = () => {
                         <a className="text-gray-800" href={appRoutes.base}>Home</a>
                     </li>
                     <li>
-                        <a className="text-gray-800" href={appRoutes.tastings}>Tastings</a>
+                        <a className="text-gray-800" href={appRoutes.tastings.base}>Tastings</a>
                     </li>
                     <li>
                         <a className="text-gray-800" href={appRoutes.contact}>Contact</a>
@@ -56,7 +56,7 @@ const NavBar = () => {
                     <motion.div animate={{ opacity: 1, x: 0}} initial={{ opacity: 0, x: 25}} className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
                         <div className="flex flex-col gap-24 text-xl font-light">
                             <a className="text-gray-800" href={appRoutes.base}>Home</a>
-                            <a className="text-gray-800" href={appRoutes.tastings}>Tastings</a>
+                            <a className="text-gray-800" href={appRoutes.tastings.base}>Tastings</a>
                             <a className="text-gray-800" href={appRoutes.contact}>Contact</a>
                         </div>
                     </motion.div>
--- src/config/url.const.ts
+++ src/config/url.const.ts
@@ -1,5 +1,11 @@
 export const appRoutes = {
     base: '/',
     contact: '/contact',
-    tastings: '/tastings',
+    tastings: {
+        base: '/tastings',
+        whisky: '/tastings/whisky',
+        rum: '/tastings/rum',
+        tequila: '/tastings/tequila-mezcal',
+        wine: '/tastings/wine',
+    },
 }
