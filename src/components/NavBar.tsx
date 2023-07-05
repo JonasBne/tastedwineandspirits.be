a7bdd9c10d8373339b049af02c17ef3b1578383d
Jonas Boone <boone.jonas@outlook.be>
2023-07-05 21:20:57 +0200

    fix: remove usehooks-ts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
-import { useMediaQuery } from "usehooks-ts";
 import { motion } from "framer-motion";
 import { appRoutes } from "../config/url.const";
 import Logo from "/assets/logos/tasted.svg";
 
 const NavBar = () => {
-    const isMobile = useMediaQuery("(max-width: 1024px)");
     const [initialRenderComplete, setInitialRenderComplete] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
 
@@ -26,42 +24,38 @@ const NavBar = () => {
                 <img alt="Tasted logo" src={Logo} className="h-10" />
             </a>
 
-            {
-                !isMobile && (
-                    <ul className="flex gap-x-12 list-none">
-                    <li>
-                        <a className="text-slate-800" href={appRoutes.base}>Home</a>
-                    </li>
-                    <li>
-                        <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
-                    </li>
-                    <li>
-                        <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
-                    </li>
-                </ul>
-                )
-            }
 
-            {
-                isMobile && (
-                    <div className="space-y-1.5 cursor-pointer z-50" onClick={() => setShowMenu(prev => !prev)}>
-                    <motion.span animate={{ rotateZ: showMenu ? 45 : 0, y: showMenu ? 8 : 0 }} className="block h-0.5 w-8 bg-gray-800"></motion.span>
-                    <motion.span animate={{ width: showMenu ? 0 : 24 }} className="block h-0.5 w-6 bg-gray-800"></motion.span>
-                    <motion.span animate={{ rotateZ: showMenu ? -45 : 0, y: showMenu ? -8 : 0, width: showMenu ? 32 : 16 }} className="block h-0.5 w-4 bg-gray-800"></motion.span>
-                </div>
-                )
-            }
-            {
-                isMobile && showMenu && (
-                    <motion.div animate={{ opacity: 1, x: 0}} initial={{ opacity: 0, x: 25}} className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
-                        <div className="flex flex-col gap-24 text-3xl font-semibold">
-                            <a className="text-slate-800" href={appRoutes.base}>Home</a>
-                            <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
-                            <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
-                        </div>
-                    </motion.div>
-                )
-            }
+            <ul className="hidden lg:flex lg:gap-x-12 list-none">
+            <li>
+                <a className="text-slate-800" href={appRoutes.base}>Home</a>
+            </li>
+            <li>
+                <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
+            </li>
+            <li>
+                <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
+            </li>
+        </ul>
+
+
+
+        <div className="space-y-1.5 cursor-pointer z-50 lg:hidden" onClick={() => setShowMenu(prev => !prev)}>
+            <motion.span animate={{ rotateZ: showMenu ? 45 : 0, y: showMenu ? 8 : 0 }} className="block h-0.5 w-8 bg-gray-800"></motion.span>
+            <motion.span animate={{ width: showMenu ? 0 : 24 }} className="block h-0.5 w-6 bg-gray-800"></motion.span>
+            <motion.span animate={{ rotateZ: showMenu ? -45 : 0, y: showMenu ? -8 : 0, width: showMenu ? 32 : 16 }} className="block h-0.5 w-4 bg-gray-800"></motion.span>
+        </div>
+
+        {
+            showMenu && (
+                <motion.div animate={{ opacity: 1, x: 0}} initial={{ opacity: 0, x: 25}} className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center lg:hidden">
+                    <div className="flex flex-col gap-24 text-3xl font-semibold">
+                        <a className="text-slate-800" href={appRoutes.base}>Home</a>
+                        <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
+                        <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
+                    </div>
+                </motion.div>
+            )
+        }
         </nav>
     )
 }
