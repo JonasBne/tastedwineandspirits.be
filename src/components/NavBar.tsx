50e249438c90c2073106f64deb4f7c4f19e4aeed
Jonas Boone <boone.jonas@outlook.be>
2023-07-05 19:51:31 +0200

    style: change color and margin homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,20 +23,20 @@ const NavBar = () => {
     return (
         <nav className="bg-gray-100 flex items-center justify-between">
             <a href={appRoutes.base}>
-                <img alt="test" src={Logo} className="h-10" />
+                <img alt="Tasted logo" src={Logo} className="h-10" />
             </a>
 
             {
                 !isMobile && (
                     <ul className="flex gap-x-12 list-none">
                     <li>
-                        <a className="text-gray-800" href={appRoutes.base}>Home</a>
+                        <a className="text-slate-800" href={appRoutes.base}>Home</a>
                     </li>
                     <li>
-                        <a className="text-gray-800" href={appRoutes.tastings.base}>Tastings</a>
+                        <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
                     </li>
                     <li>
-                        <a className="text-gray-800" href={appRoutes.contact}>Contact</a>
+                        <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
                     </li>
                 </ul>
                 )
@@ -55,9 +55,9 @@ const NavBar = () => {
                 isMobile && showMenu && (
                     <motion.div animate={{ opacity: 1, x: 0}} initial={{ opacity: 0, x: 25}} className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
                         <div className="flex flex-col gap-24 text-xl font-light">
-                            <a className="text-gray-800" href={appRoutes.base}>Home</a>
-                            <a className="text-gray-800" href={appRoutes.tastings.base}>Tastings</a>
-                            <a className="text-gray-800" href={appRoutes.contact}>Contact</a>
+                            <a className="text-slate-800" href={appRoutes.base}>Home</a>
+                            <a className="text-slate-800" href={appRoutes.tastings.base}>Tastings</a>
+                            <a className="text-slate-800" href={appRoutes.contact}>Contact</a>
                         </div>
                     </motion.div>
                 )
